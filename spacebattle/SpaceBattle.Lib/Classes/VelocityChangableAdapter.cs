8636b1e7b713e33fba380de856c0a1b61a52f420
Daria <ladydashaberezina@mail.ru>
2025-07-31 17:18:01 +0600

    add VelocityChangableAdapater

---
+++ spacebattle/SpaceBattle.Lib/Classes/VelocityChangableAdapter.cs
@@ -0,0 +1,17 @@
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class VelocityChangableAdpater : IVelocityChangable
+{
+    private readonly IUObject _uObject;
+    public VelocityChangableAdpater(IUObject uObject)
+    {
+        _uObject = uObject;
+    }
+    public Vector Velocity
+    {
+        get => IoC.Resolve<Vector>("VelocityChangable.Velocity.Get", _uObject);
+        set => IoC.Resolve<ICommand>("VelocityChangable.Velocity.Set", _uObject, value).Execute();
+    }
+}
