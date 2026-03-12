8b746d09abcdecc98a21f0d5db4b07ef5ab5d77f
Daria <ladydashaberezina@mail.ru>
2026-03-12 23:40:07 +0600

    add class AdapterAttribute

---
+++ spacebattle/SpaceBattle.Lib/Classes/AdapterAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceBattle.Lib;
+
+[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class, AllowMultiple = true)]
+public class AdapterAttribute : Attribute
+{
+    public Type TargetType { get; }
+    public string PropertyName { get; }
+    public string StrategyName { get; }
+
+    public AdapterAttribute(Type targetType, string propertyName, string strategyName)
+    {
+        TargetType = targetType;
+        PropertyName = propertyName;
+        StrategyName = strategyName;
+    }
+}
