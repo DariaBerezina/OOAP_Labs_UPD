c7d6941d146497d53973e84ca12beee3d727af6e
Daria <ladydashaberezina@mail.ru>
2026-03-17 03:10:37 +0600

    add class InitUniversalDiStrategyCommand

---
+++ spacebattle/SpaceBattle.Lib/Classes/InitUniversalDiStrategyCommand.cs
@@ -0,0 +1,30 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class InitUniversalDiStrategyCommand : ICommand
+{
+    public void Execute()
+    {
+        var argumentResolvers = new Dictionary<bool, Func<Type, IUObject, object>>
+        {
+            {true, (type, uObj) => IoC.Resolve<object>("Adapter", type, uObj)},
+
+            {false, (type, uObj) => IoC.Resolve<object>(type.Name, uObj)}
+        };
+
+        IoC.Resolve<ICommand>("IoC.Register", "IoC.CreateInstance", (object[] args) =>
+        {
+            var targetType = (Type)args[0];
+            var uObject = (IUObject)args[1];
+
+            var constructor = targetType.GetConstructors().Single();
+
+            var resolvedArgs = constructor.GetParameters()
+                .Select(p => argumentResolvers[p.ParameterType.IsInterface](p.ParameterType, uObject))
+                .ToArray();
+
+            return constructor.Invoke(resolvedArgs);
+        }).Execute();
+    }
+}
