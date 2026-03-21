f358781cdeee7370aaa365e16ee2d4ca873e1894
Daria <ladydashaberezina@mail.ru>
2026-03-21 19:42:34 +0600

    add class GameCommand

---
+++ spacebattle/SpaceBattle.Lib/Classes/GameCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class GameCommand : ICommand
+{
+    private readonly object _gameScope;
+
+    public GameCommand(object gameScope)
+    {
+        _gameScope = gameScope;
+    }
+
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", _gameScope).Execute();
+
+        var quantum = IoC.Resolve<TimeSpan>("Game.TimeQuantum");
+        var queue = IoC.Resolve<IQueue>("Game.Queue");
+
+        var stopwatch = Stopwatch.StartNew();
+
+        while (stopwatch.Elapsed < quantum)
+        {
+            var command = queue.Take();
+
+            try
+            {
+                command.Execute();
+            }
+            catch (Exception ex)
+            {
+                var handler = IoC.Resolve<ICommand>("Exception.Handler", command, ex);
+                handler.Execute();
+            }
+        }
+
+        stopwatch.Stop();
+    }
+}
