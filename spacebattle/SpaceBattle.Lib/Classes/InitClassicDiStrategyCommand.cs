3eba7f795b1db0eaff3d1798471642e416a61988
Daria <ladydashaberezina@mail.ru>
2026-03-23 01:08:14 +0600

    create InitClassicDiStrategyCommand & testing it

---
+++ spacebattle/SpaceBattle.Lib/Classes/InitClassicDiStrategyCommand.cs
@@ -0,0 +1,21 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class InitClassicDiStrategyCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "IoC.CreateInstance", (object[] args) =>
+        {
+            var targetType = (Type)args[0];
+            var contextArgs = args.Skip(1).ToArray();
+            var constructor = targetType.GetConstructors().Single();
+            var resolvedArgs = constructor.GetParameters()
+                .Select(p => IoC.Resolve<object>(p.ParameterType.Name, contextArgs))
+                .ToArray();
+
+            return constructor.Invoke(resolvedArgs);
+        }).Execute();
+    }
+}
