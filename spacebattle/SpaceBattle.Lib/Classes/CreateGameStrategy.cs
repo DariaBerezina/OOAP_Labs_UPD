9f7a428da73353f46d110e04431b85d8a9ab6c78
Daria <ladydashaberezina@mail.ru>
2026-03-22 17:23:15 +0600

    add static class CreateGameStrategy

---
+++ spacebattle/SpaceBattle.Lib/Classes/CreateGameStrategy.cs
@@ -0,0 +1,20 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public static class CreateGameStrategy
+{
+    private const string ScopesCurrentSet = "Scopes.Current.Set";
+
+    public static object Resolve(object[] args)
+    {
+        var timeQuantum = (TimeSpan)args[0];
+        var parentScope = IoC.Resolve<object>("Scopes.Current");
+        var gameScope = IoC.Resolve<object>("Scopes.New", parentScope);
+        IoC.Resolve<Hwdtech.ICommand>(ScopesCurrentSet, gameScope).Execute();
+        new SetupGameScopeCommand(timeQuantum).Execute();
+        IoC.Resolve<Hwdtech.ICommand>(ScopesCurrentSet, parentScope).Execute();
+
+        return new GameCommand(gameScope);
+    }
+}
