3cf2601f2377225d86722a7858336179a8e20053
Daria <ladydashaberezina@mail.ru>
2026-03-22 23:25:08 +0600

    format SetupGameScopeCommand & add tests GameStrategiesTests

--- spacebattle/SpaceBattle.Lib/Classes/SetupGameScopeCommand.cs
+++ spacebattle/SpaceBattle.Lib/Classes/SetupGameScopeCommand.cs
@@ -14,16 +14,50 @@ public class SetupGameScopeCommand : ICommand
 
     public void Execute()
     {
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.TimeQuantum", (object[] a) => _timeQuantum).Execute();
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.TimeQuantum",
+            (Func<object[], object>)((object[] a) => _timeQuantum)
+        ).Execute();
 
         var gameQueue = new GameQueue();
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.Queue", (object[] a) => gameQueue).Execute();
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.Queue.Push", (object[] a) => new ActionCommand(() => gameQueue.Add((ICommand)a[0]))).Execute();
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.Queue.Take", (object[] a) => gameQueue.Take()).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.Queue",
+            (Func<object[], object>)((object[] a) => gameQueue)
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.Queue.Push",
+            (Func<object[], object>)((object[] a) => new ActionCommand(() => gameQueue.Add((ICommand)a[0])))
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.Queue.Take",
+            (Func<object[], object>)((object[] a) => gameQueue.Take())
+        ).Execute();
 
         var repository = new GameObjectRepository();
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.Repository", (object[] a) => repository).Execute();
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.Objects.Get", (object[] a) => repository.Get((string)a[0])).Execute();
-        IoC.Resolve<Hwdtech.ICommand>(IocRegister, "Game.Objects.Delete", (object[] a) => new ActionCommand(() => repository.Remove((string)a[0]))).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.Repository",
+            (Func<object[], object>)((object[] a) => repository)
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.Objects.Get",
+            (Func<object[], object>)((object[] a) => repository.Get((string)a[0]))
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            IocRegister,
+            "Game.Objects.Delete",
+            (Func<object[], object>)((object[] a) => new ActionCommand(() => repository.Remove((string)a[0])))
+        ).Execute();
     }
 }
