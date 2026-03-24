90ddb546002886bb4ecbd7466f151875f904cfa0
Daria <ladydashaberezina@mail.ru>
2026-03-24 16:09:20 +0600

    feat: add adapter generation (from lab_adapters_generator)

---
+++ spacebattle/SpaceBattle.Lib/Classes/InitAdapterGenerationCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class InitAdapterGenerationCommand : ICommand
+{
+    public void Execute()
+    {
+        var generatedTypeCache = new ConcurrentDictionary<Type, Type>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Adapter", (object[] args) =>
+        {
+            var targetInterface = (Type)args[0];
+            var targetObj = (IUObject)args[1];
+
+            var adapterType = generatedTypeCache.GetOrAdd(targetInterface,
+            (interf) =>
+            {
+                var code = AdapterCodeGenerator.Generate(interf);
+                var baseName = interf.Name.StartsWith('I') ? interf.Name.Substring(1) : interf.Name;
+                var className = $"{baseName}Adapter";
+
+                return AdapterCompiler.Compile(code, className);
+            });
+
+            return Activator.CreateInstance(adapterType, targetObj)!;
+        }).Execute();
+    }
+}
